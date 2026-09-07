4df658adb1b7a26c0f3d28a9496acca589b8dba1
hirad990 <iamhirad17@gmail.com>
2026-09-07 13:25:58 +0330

    fix frontend API base URL for production backend

--- BCN/binercraft/binercraft-frontend/src/main.jsx
+++ BCN/binercraft/binercraft-frontend/src/main.jsx
@@ -1,10 +1,13 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 import './styles/index.css'
+
+axios.defaults.baseURL = 'https://binercraft.ir/fozing'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
